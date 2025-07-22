0a5ca35e5de467a111035f97a91b8b5aff136481
Nicolas AMATI <skkyz3r.dev@gmail.com>
2025-07-22 17:42:52 +0200

    remove unnecessary config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,19 +9,4 @@ export default defineNuxtConfig({
     plugins: [tailwindcss()],
   },
   css: ["~/assets/css/main.css"],
-  runtimeConfig: {
-    session: {
-      password: process.env.NUXT_SESSION_PASSWORD || "",
-    },
-    oauth: {
-      github: {
-        clientId: process.env.NUXT_OAUTH_GITHUB_CLIENT_ID || "",
-        clientSecret: process.env.NUXT_OAUTH_GITHUB_CLIENT_SECRET || "",
-      },
-      discord: {
-        clientId: process.env.NUXT_OAUTH_DISCORD_CLIENT_ID || "",
-        clientSecret: process.env.NUXT_OAUTH_DISCORD_CLIENT_SECRET || "",
-      },
-    },
-  },
 });
